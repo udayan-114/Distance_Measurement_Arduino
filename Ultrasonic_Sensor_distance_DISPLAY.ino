2ced292fc9e8df64cc9ceeea4ddf83a28fff5da4
udayan-114 <55526884+udayan-114@users.noreply.github.com>
2019-09-19 20:46:02 +0530

    Add files via upload

---
+++ Ultrasonic_Sensor_distance_DISPLAY.ino
@@ -0,0 +1,25 @@
+#define echoPin 11
+#define trigPin 12
+long duration, Distance;
+void setup()
+{
+  Serial.begin (9600);
+  pinMode (trigPin, OUTPUT);
+  pinMode (echoPin, INPUT);
+}
+ void loop()
+ {
+
+  digitalWrite (trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite (trigPin, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite (trigPin, LOW);
+  duration = pulseIn (echoPin, HIGH);
+  Distance = duration*0.034/2;
+
+  Serial.print ("Distance:");
+  Serial.println(Distance);
+  delay(50);
+ } 
